01e58e24105bba6357d546de94e2cf3ef53be981
hongyuue <hongyuue@163.com>
2022-10-21 00:05:23 +0800

    vite 配置

--- webgl01/src/utils.js
+++ webgl01/src/utils.js
@@ -1,3 +1,12 @@
 export function createBufferObject(glEnum, size, bufferData, usage) {
-
+    const vbo = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    var verties = new Float32Array([
+        -0.5, -0.5, -2.0, 0.6, 0, 0,
+        0.5, -0.5, -2.0, 0, 0.7, 0,
+        0.0, 0.5, -2.0, 0, 0, 0.5,
+    ]);
+    const FSIZE = verties.BYTES_PER_ELEMENT
+    gl.bufferData(gl.ARRAY_BUFFER, verties, gl.STATIC_DRAW);
+    return vbo
 }
