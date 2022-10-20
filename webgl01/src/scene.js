72986e3db52533d72ece0228cb916fb107703227
hongyuue <hongyuue@163.com>
2022-10-20 23:04:02 +0800

    rgb 三角形

--- webgl01/src/scene.js
+++ webgl01/src/scene.js
@@ -6,6 +6,7 @@ const width = canvas.clientWidth
 const height = canvas.clientHeight
 let program
 let vbo
+let color_vbo
 var radius = 200;
 var cameraAngleRadians = degToRad(0);
 var fieldOfViewRadians = degToRad(45);
@@ -27,21 +28,29 @@ export function init() {
     vbo = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     var positions = [
-        -0.5, -0.5, -2.0, 1.0,
-        0.5, -0.5, -2.0, 1.0,
-        0.0, 0.5, -2.0, 1.0,
+        -0.5, -0.5, -2.0,
+        0.5, -0.5, -2.0,
+        0.0, 0.5, -2.0,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+
+    color_vbo = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_vbo);
+    var colors = [
+        0.6, 0, 0,
+        0, 0.7, 0,
+        0, 0, 0.5,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
     program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
 }
 
 export function render() {
-    gl.clearColor(0.1, 0.4, 0.6, 1.0)
+    gl.clearColor(0.1, 0.1, 0.1, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
     gl.enable(gl.CULL_FACE);
-    // Enable the depth buffer
     gl.enable(gl.DEPTH_TEST);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
     gl.useProgram(program)
     const M_location = gl.getUniformLocation(program, "M");
     const V_location = gl.getUniformLocation(program, "V");
@@ -51,15 +60,20 @@ export function render() {
     gl.uniformMatrix4fv(V_location, false, view_matrix)
     gl.uniformMatrix4fv(P_location, false, projection_matrix)
 
-    const p = gl.getAttribLocation(program, 'position')
-    gl.enableVertexAttribArray(p)
-    gl.vertexAttribPointer(p, 4, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
+    const a_position = gl.getAttribLocation(program, 'position')
+    gl.enableVertexAttribArray(a_position)
+    gl.vertexAttribPointer(a_position, 3, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
 
-    const color_pos = gl.getAttribLocation(program, 'color')
-    // gl.enableVertexAttribArray(color_pos)
-    // gl.vertexAttribPointer(p, 3, gl.FLOAT, false, 0, 3);
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_vbo)
+    const a_color = gl.getAttribLocation(program, 'color')
+    gl.enableVertexAttribArray(a_color)
+    gl.vertexAttribPointer(a_color, 3, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
 
     gl.drawArrays(gl.TRIANGLES, 0, 3); /// 画三角形，从第几个点开始 绘制几个点
     // gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    // glUseProgram(0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+    gl.useProgram(null);
 }
