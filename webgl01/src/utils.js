fbfac9161f125322ae06945c535c58633b8316e4
hongyuue <hongyuue@163.com>
2022-10-22 23:08:58 +0800

    修复颜色和材质重合的问题

--- webgl01/src/utils.js
+++ webgl01/src/utils.js
@@ -13,15 +13,13 @@ export function createTexture2DFromImg(image) {
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
         new Uint8Array([0, 0, 255, 255]));
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.generateMipmap(gl.TEXTURE_2D);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-
-    gl.generateMipmap(gl.TEXTURE_2D);
     return texture
 }
 
@@ -36,4 +34,10 @@ export async function createTextureFromUrl(path) {
             resolve(texture)
         });
     })
+}
+
+export async function loadFileFromUrl(path) {
+    const response = await fetch(path);
+    const text = await response.text();
+    return text
 }
